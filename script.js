158f70f1f482f89a572d8ac1a633225a752207e7
Julio-sinistro69 <juliodesouza2077@gmail.com>
2025-09-15 09:38:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ let maxima_rod = 7;
 function comecarJogo() {
 rodada = 0
 }
+function pedra(){
+    let pedra = 1;
+    alert("skibidi");
+}
+    function tesoura() {
+        let tesoura = 2;   
+        alert("tesoura");
+    }
+        function papel() {
+            let papel = 3 ; 
+            alert("ddd");  
+        }
+            
